9b891f0219745b18e0e75ddc8b3cefd524db3814
Pavel <pashtetrus@gmail.com>
2025-03-11 21:56:05 -0300

    init commit

--- src/main/java/ru/skillbox/social_network_bot/controller/WebhookController.java
+++ src/main/java/ru/skillbox/social_network_bot/controller/WebhookController.java
@@ -1,11 +1,13 @@
 package ru.skillbox.social_network_bot.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import ru.skillbox.social_network_bot.service.TelegramBot;
 
+@Slf4j
 @RestController
 @RequestMapping("/")
 @RequiredArgsConstructor
@@ -13,8 +15,12 @@ public class WebhookController {
 
     private final TelegramBot telegramBot;
 
+
     @PostMapping("/webhook")
     public BotApiMethod<?> onUpdateReceived(@RequestBody Update update) {
+
+        log.warn("Received update: {}", update);
+
         return telegramBot.onWebhookUpdateReceived(update);
     }
 }
