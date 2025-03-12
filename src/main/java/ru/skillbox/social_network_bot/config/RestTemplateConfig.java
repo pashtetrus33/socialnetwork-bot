483376711a3038316f84d38fcb13d8711adc4ea7
Pavel <pashtetrus@gmail.com>
2025-03-11 22:43:24 -0300

    init commit

--- src/main/java/ru/skillbox/social_network_bot/config/RestTemplateConfig.java
+++ src/main/java/ru/skillbox/social_network_bot/config/RestTemplateConfig.java
@@ -1,3 +1,5 @@
+package ru.skillbox.social_network_bot.config;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
