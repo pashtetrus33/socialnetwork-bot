ed2d7253969758262aa03e6843db93b7834849cd
Pavel <pashtetrus@gmail.com>
2025-03-12 22:44:36 -0300

    refactoring

--- src/main/java/ru/skillbox/social_network_bot/client/AuthServiceClient.java
+++ src/main/java/ru/skillbox/social_network_bot/client/AuthServiceClient.java
@@ -1,8 +1,9 @@
 package ru.skillbox.social_network_bot.client;
 
+import feign.Body;
+import feign.Headers;
 import feign.Param;
 import feign.RequestLine;
-import org.springframework.web.bind.annotation.RequestBody;
 import ru.skillbox.social_network_bot.dto.AuthenticateRq;
 import ru.skillbox.social_network_bot.dto.TokenResponse;
 
@@ -11,6 +12,9 @@ public interface AuthServiceClient {
     @RequestLine("GET /validate?token={token}")
     Boolean validateToken(@Param("token") String token);
 
+
     @RequestLine("POST /login")
+    @Headers("Content-Type: application/json")
     TokenResponse login(AuthenticateRq request);
+
 }
