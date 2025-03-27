ef4b735e7b93bb5b0ed364c4ea66c72649d7f321
Pavel <pashtetrus@gmail.com>
2025-03-27 14:12:08 -0300

    refactoring

--- src/main/java/ru/skillbox/social_network_bot/dto/PagePostDto.java
+++ src/main/java/ru/skillbox/social_network_bot/dto/PagePostDto.java
@@ -1,25 +1,22 @@
 package ru.skillbox.social_network_bot.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PositiveOrZero;
 import lombok.*;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
-import ru.skillbox.social_network_bot.config.PageableDeserializer;
-import ru.skillbox.social_network_bot.config.SortDeserializer;
 
 import java.util.List;
 
+
+@JsonIgnoreProperties(ignoreUnknown = true)  // Игнорировать неизвестные поля
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-@JsonIgnoreProperties(ignoreUnknown = true)  // Игнорировать неизвестные поля
 public class PagePostDto {
 
     @NotNull
@@ -40,7 +37,6 @@ public class PagePostDto {
 
     private List<PostDto> content;
 
-    @JsonDeserialize(using = SortDeserializer.class)
     private Sort sort;
 
     @NotNull
@@ -53,13 +49,8 @@ public class PagePostDto {
     @Min(0)
     private Integer numberOfElements;
 
-    @JsonDeserialize(using = PageableDeserializer.class)
     private Pageable pageable;
 
     @NotNull
     private Boolean empty;
-
-    // Обрабатываем поля внутри Pageable
-    @JsonProperty("pageSize")  // Убедитесь, что это поле есть внутри pageable
-    private Integer pageSize;
 }
